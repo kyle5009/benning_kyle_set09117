6161d6a950da7b9611b0a7f5d851b106ab2c5407
Kyle <benning97@gmail.com>
2017-10-25 17:17:52 +0100

    Able to append the grid and input coordinates

--- checkers.py
+++ checkers.py
@@ -13,4 +13,26 @@ def printBoard():
         print('|' + board[i][0] + '|' + board[i][1] + '|' + board[i][2] + '|' + board[i][3] + '|' + board[i][4] + '|' + board[i][5] +
                 '|' + board[i][6] + '|' + board[i][7] + '|')
 
+print ('select the piece you wish to move')
+turn = 'r'
+move = input()
+coord_A = move.split(",")
+coord_Ax = int(coord_A[0])
+coord_Ay = int(coord_A[1])
+board[coord_Ay][coord_Ax] = turn
+
+#Board[][] = turn
+if turn == 'r':
+    turn = 'b'
+else:
+    turn = 'r'
+
+def move():
+
+    if board == '_':
+        print('you must select a valid piece')
+
+
+
+
 printBoard()
