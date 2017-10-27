e1e2558509f3f7724da6371bde3d739723c6f117
Kyle <benning97@gmail.com>
2017-10-27 17:33:33 +0100

    continued working on making the pieces moves

--- checkers.py
+++ checkers.py
@@ -13,27 +13,34 @@ def printBoard():
         print('|' + board[i][0] + '|' + board[i][1] + '|' + board[i][2] + '|' + board[i][3] + '|' + board[i][4] + '|' + board[i][5] +
                 '|' + board[i][6] + '|' + board[i][7] + '|')
 
+printBoard()
+
 print ('select the piece you wish to move')
-turn = 'r'
-move = input()
 coord_A = move.split(",")
 coord_Ax = int(coord_A[0])
 coord_Ay = int(coord_A[1])
-board[coord_Ay][coord_Ax] = turn
-
-
+board[coord_Ay][coord_Ax] = '_'
+#board[coord_Ay][coord_Ax] = turn
 #Board[][] = turn
 if turn == 'r':
     turn = 'b'
 else:
     turn = 'r'
 
+printBoard()
+
+print ('select where you wish to move that piece ')
+move2 = input()
+coord_A2 = move2.split(",")
+coord_A2x = int(coord_A2[0])
+coord_A2y = int(coord_A2[1])
+board[coord_A2y][coord_A2x] = turn
+
+print ("You have moved a piece!")
+
 def move():
 
     if board == '_':
         print('you must select a valid piece')
-
-
-
 
 printBoard()
