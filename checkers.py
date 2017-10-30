2d7f8b13b480c55380a79bf9f5bd96b80d73696e
Kyle <benning97@gmail.com>
2017-10-30 16:24:37 +0000

    cleaned up the board, turn system functioning , stoped user from selecting blank spaces to move from

--- checkers.py
+++ checkers.py
@@ -8,39 +8,53 @@ board = [['_','b','_','b','_','b','_','b'],
          ['r','_','r','_','r','_','r','_']]
 
 def printBoard():
+    print('   ' + '0' + ' ' + '1' + ' ' + '2' + ' ' + '3' + ' ' + '4' + ' ' + '5' + ' ' + '6' + ' ' + '7' + ' ')
+    print('   ' + '_' + ' ' + '_' + ' ' + '_' + ' ' + '_' + ' ' + '_' + ' ' + '_' + ' ' + '_' + ' ' + '_' + ' ' )
     i = 0
     for i in range(0,8):
-        print('|' + board[i][0] + '|' + board[i][1] + '|' + board[i][2] + '|' + board[i][3] + '|' + board[i][4] + '|' + board[i][5] +
+        print(str(i) + ' ' + '|' + board[i][0] + '|' + board[i][1] + '|' + board[i][2] + '|' + board[i][3] + '|' + board[i][4] + '|' + board[i][5] +
                 '|' + board[i][6] + '|' + board[i][7] + '|')
+
+def checkNotBlank(x,y):
+    if board[y][x] == '_':
+        print('you must select a valid piece')
+        return False
+    else:
+        return True
+
+def changeTurn():
+    global turn
+    if turn == 'r':
+        turn = 'b'
+    else:
+        turn = 'r'
+
+#def notYourPiece
+
 
 printBoard()
 
 turn = 'r'
-print ('select the piece you wish to move')
-move = input()
-coord_A = move.split(",")
-coord_Ax = int(coord_A[0])
-coord_Ay = int(coord_A[1])
-board[coord_Ay][coord_Ax] = '_'
+gameWinner = True
+while gameWinner == True:
+    print ('select the piece you wish to move')
+    move = input()
+    coord_A = move.split(",")
+    coord_Ax = int(coord_A[0])
+    coord_Ay = int(coord_A[1])
+    if checkNotBlank(coord_Ax,coord_Ay) == True:
+        board[coord_Ay][coord_Ax] = '_'
 
-if turn == 'r':
-    turn = 'b'
-else:
-    turn = 'r'
+    print ('select where you wish to move that piece ')
+    move2 = input()
+    coord_A2 = move2.split(",")
+    coord_A2x = int(coord_A2[0])
+    coord_A2y = int(coord_A2[1])
+    board[coord_A2y][coord_A2x] = turn
 
-print ('select where you wish to move that piece ')
-move2 = input()
-coord_A2 = move2.split(",")
-coord_A2x = int(coord_A2[0])
-coord_A2y = int(coord_A2[1])
-board[coord_A2y][coord_A2x] = turn
+    print ("You have moved a piece!")
 
-print ("You have moved a piece!")
-
-def move():
-
-    if board == '_':
-        print('you must select a valid piece')
-
+    changeTurn()
+    printBoard()
 
 printBoard()
