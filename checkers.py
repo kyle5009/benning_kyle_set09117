f117f35032cac014b6bc7ca69ee350f5dcfce418
Kyle <benning97@gmail.com>
2017-11-16 22:48:32 +0000

    minor bug fixes

--- checkers.py
+++ checkers.py
@@ -80,10 +80,8 @@ def moveOne_kb1(coord_Ax,coord_Ay,coord_A2x,coord_A2y):
                 print('Illegal move')
                 return False
         else:
-            print ('1')
             return True
     else:
-        print('2')
         return True
 
 #makes sure that black pieces can only move one space diagonally, down the board
@@ -110,10 +108,8 @@ def moveOne_kr1(coord_Ax,coord_Ay,coord_A2x,coord_A2y):
                 print('Illegal move')
                 return False
         else:
-            print ('1')
             return True
     else:
-        print('2')
         return True
 
 #allows red pieces to jump and take black pieces
@@ -141,7 +137,7 @@ def jump_r2(coord_Ax,coord_Ay):
             if board[coord_Ay - 2][coord_Ax - 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay - 1][coord_Ax - 1] = '_'
-                board[coord_Ay - 2][coord_Ax - 2] = 'b'
+                board[coord_Ay - 2][coord_Ax - 2] = 'r'
                 changeTurn()
                 printBoard()
                 return True
@@ -159,7 +155,7 @@ def jump_b1(coord_Ax,coord_Ay):
             if board[coord_Ay + 2][coord_Ax - 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay + 1][coord_Ax - 1] = '_'
-                board[coord_Ay + 2][coord_Ax - 2] = board[coord_Ay][coord_Ax]
+                board[coord_Ay + 2][coord_Ax - 2] = 'b'
                 changeTurn()
                 printBoard()
                 return True
@@ -177,7 +173,7 @@ def jump_b2(coord_Ax,coord_Ay):
             if board[coord_Ay + 2][coord_Ax + 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay + 1][coord_Ax + 1] = '_'
-                board[coord_Ay + 2][coord_Ax + 2] = board[coord_Ay][coord_Ax]
+                board[coord_Ay + 2][coord_Ax + 2] = 'b'
                 changeTurn()
                 printBoard()
                 return True
@@ -188,15 +184,15 @@ def jump_b2(coord_Ax,coord_Ay):
     else:
         return True
 
-# ______________________________________________________________________________________________
+#king jumps
 
 def jump_kr1(coord_Ax,coord_Ay):
     if turn == 'r':
-        if board[coord_Ay - 1][coord_Ax + 1] == 'B':
+        if board[coord_Ay - 1][coord_Ax + 1] == 'b':
             if board[coord_Ay - 2][coord_Ax + 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay - 1][coord_Ax + 1] = '_'
-                board[coord_Ay - 2][coord_Ax + 2] = board[coord_Ay][coord_Ax]
+                board[coord_Ay - 2][coord_Ax + 2] = 'R'
                 changeTurn()
                 printBoard()
                 return True
@@ -209,11 +205,11 @@ def jump_kr1(coord_Ax,coord_Ay):
 
 def jump_kr2(coord_Ax,coord_Ay):
     if turn == 'r':
-        if board[coord_Ay - 1][coord_Ax - 1] == 'B':
+        if board[coord_Ay - 1][coord_Ax - 1] == 'b':
             if board[coord_Ay - 2][coord_Ax - 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay - 1][coord_Ax - 1] = '_'
-                board[coord_Ay - 2][coord_Ax - 2] = board[coord_Ay][coord_Ax]
+                board[coord_Ay - 2][coord_Ax - 2] = 'R'
                 changeTurn()
                 printBoard()
                 return True
@@ -226,11 +222,11 @@ def jump_kr2(coord_Ax,coord_Ay):
 
 def jump_kb1(coord_Ax,coord_Ay):
     if turn == 'b':
-        if board[coord_Ay + 1][coord_Ax - 1] == 'R':
+        if board[coord_Ay + 1][coord_Ax - 1] == 'r':
             if board[coord_Ay + 2][coord_Ax - 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay + 1][coord_Ax - 1] = '_'
-                board[coord_Ay + 2][coord_Ax - 2] = board[coord_Ay][coord_Ax]
+                board[coord_Ay + 2][coord_Ax - 2] = 'R'
                 changeTurn()
                 printBoard()
                 return True
@@ -243,11 +239,11 @@ def jump_kb1(coord_Ax,coord_Ay):
 
 def jump_kb2(coord_Ax,coord_Ay):
     if turn == 'b':
-        if board[coord_Ay + 1][coord_Ax + 1] == 'R':
+        if board[coord_Ay + 1][coord_Ax + 1] == 'r':
             if board[coord_Ay + 2][coord_Ax + 2] == '_':
                 board[coord_Ay][coord_Ax] = '_'
                 board[coord_Ay + 1][coord_Ax + 1] = '_'
-                board[coord_Ay + 2][coord_Ax + 2] = board[coord_Ay][coord_Ax]
+                board[coord_Ay + 2][coord_Ax + 2] = 'R'
                 changeTurn()
                 printBoard()
                 return True
@@ -257,11 +253,6 @@ def jump_kb2(coord_Ax,coord_Ay):
             return True
     else:
         return True
-
-def gameWinner():
-    global gameWinner
-
-
 
 printBoard()
 
@@ -286,60 +277,64 @@ while gameWinner == True:
         if jump_r2(coord_Ax,coord_Ay) == True:
             if jump_b1(coord_Ax,coord_Ay) == True:
                 if jump_b2(coord_Ax,coord_Ay) == True:
-                    #prints the piece they selected
-                    print("At coordinate " + str(coord_Ax) + "," + str(coord_Ay) + " is an " + board[coord_Ay][coord_Ax])
-                    #checks if the functions are True
-                    if checkNotBlank(int(coord_A[0]),int(coord_A[1])) == True:
-                        if notYourPiece_R(int(coord_A[0]),int(coord_A[1])) == True:
-                            if notYourPiece_B(int(coord_A[0]),int(coord_A[1])) == True:
-                                #asks the user where they wish to move their pice
-                                print ('select where you wish to move that piece ')
-                                move2 = input()
-                                #splits the input to 2 seperate coordinates
-                                coord_A2 = move2.split(",")
-                                #converts the coordinates to integers
-                                coord_A2x = int(coord_A2[0])
-                                coord_A2y = int(coord_A2[1])
-                                #checks if the functions are true
-                                if moveOne_r1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
-                                    if moveOne_b1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
-                                       if moveOne_kb1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
-                                           if moveOne_kr1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
-                                               #moves the desired piece by setting the distination space to the piece that was moved
-                                               board[coord_A2y][coord_A2x] = board[coord_Ay][coord_Ax]
-                                               #sets the space where the piece was moved to to blank
-                                               board[coord_Ay][coord_Ax] = '_'
-                                               #informs the user that they have succesfully moved a piece
-                                               print ("You have moved a piece!")
-                                               #loop the check through the top line of the board for a red piece and kings it
-                                               for i in range(0,8):
-                                                   if board[0][i] == 'r':
-                                                       board[0][i] = 'R'
-                                               #loop the check through the bottom line of the board for a black piece and kings it
-                                               for i in range(0,8):
-                                                   if board[7][i] == 'b':
-                                                       board[7][i] = 'B'
+                    if jump_kb2(coord_Ax,coord_Ay) == True:
+                        if jump_kb2(coord_Ax,coord_Ay) == True:
+                            if jump_kr1(coord_Ax,coord_Ay) == True:
+                                if jump_kr2(coord_Ax,coord_Ay) == True:
+                                    #prints the piece they selected
+                                    print("At coordinate " + str(coord_Ax) + "," + str(coord_Ay) + " is an " + board[coord_Ay][coord_Ax])
+                                    #checks if the functions are True
+                                    if checkNotBlank(int(coord_A[0]),int(coord_A[1])) == True:
+                                        if notYourPiece_R(int(coord_A[0]),int(coord_A[1])) == True:
+                                            if notYourPiece_B(int(coord_A[0]),int(coord_A[1])) == True:
+                                                #asks the user where they wish to move their pice
+                                                print ('select where you wish to move that piece ')
+                                                move2 = input()
+                                                #splits the input to 2 seperate coordinates
+                                                coord_A2 = move2.split(",")
+                                                #converts the coordinates to integers
+                                                coord_A2x = int(coord_A2[0])
+                                                coord_A2y = int(coord_A2[1])
+                                                #checks if the functions are true
+                                                if moveOne_r1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
+                                                    if moveOne_b1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
+                                                       if moveOne_kb1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
+                                                           if moveOne_kr1(coord_Ax,coord_Ay,coord_A2x,coord_A2y) == True:
+                                                               #moves the desired piece by setting the distination space to the piece that was moved
+                                                               board[coord_A2y][coord_A2x] = board[coord_Ay][coord_Ax]
+                                                               #sets the space where the piece was moved to to blank
+                                                               board[coord_Ay][coord_Ax] = '_'
+                                                               #informs the user that they have succesfully moved a piece
+                                                               print ("You have moved a piece!")
+                                                               #loop the check through the top line of the board for a red piece and kings it
+                                                               for i in range(0,8):
+                                                                   if board[0][i] == 'r':
+                                                                       board[0][i] = 'R'
+                                                               #loop the check through the bottom line of the board for a black piece and kings it
+                                                               for i in range(0,8):
+                                                                   if board[7][i] == 'b':
+                                                                       board[7][i] = 'B'
 
-                                               #checks to see if there are any red pices left else winner is black
-                                               for i in range(0,8):
-                                                   for j in range(0,8):
-                                                       if board[j][i] == 'r' or 'R':
-                                                           gameWinner = True
-                                                       else:
-                                                           gameWinner = False
-                                                           print ('winner black')
+                                                               #checks to see if there are any red pices left else winner is black
+                                                               for i in range(0,8):
+                                                                   for j in range(0,8):
+                                                                       if board[j][i] == 'r' or 'R':
+                                                                           gameWinner = True
+                                                                       else:
+                                                                           gameWinner = False
+                                                                           print ('winner black')
 
-                                                #checks to see if there are any red pices left else winner is black
-                                               for i in range(0,8):
-                                                   for j in range(0,8):
-                                                       if board[j][i] == 'b' or 'B':
-                                                           gameWinner = True
-                                                       else:
-                                                           gameWinner = False
-                                                           print ('winner red')
-                                               #change turn function to change the turn
-                                               changeTurn()
-                                               #prints the board
-                                               printBoard()
+                                                                #checks to see if there are any red pices left else winner is black
+                                                               for i in range(0,8):
+                                                                   for j in range(0,8):
+                                                                       if board[j][i] == 'b' or 'B':
+                                                                           gameWinner = True
+                                                                       else:
+                                                                           gameWinner = False
+                                                                           print ('winner red')
+                                                               #change turn function to change the turn
+                                                               changeTurn()
+                                                               #prints the board
+                                                               printBoard()
 
     printBoard()
